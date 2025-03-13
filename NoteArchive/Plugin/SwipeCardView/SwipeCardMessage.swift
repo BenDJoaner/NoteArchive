e77c33a729a67ad14ed9d18535932335c3551f23
BenDJoaner <bendjoaner@outlook.com>
2025-03-14 00:42:31 +0800

    Fix UI

---
+++ NoteArchive/Plugin/SwipeCardView/SwipeCardMessage.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  iMessageCardSwipe1213
+//
+//  Created by Lurich on 2022/12/13.
+//
+
+import SwiftUI
+
+struct SwipeCardMessage: Identifiable, Equatable {
+    var id: String = UUID().uuidString
+    var imageFile: String
+}
+
